6023bd71db8b812b3528776246e6376927a37a7a
Dave Brosius <dbrosius@mebigfatguy.com>
2011-01-18 20:31:36 -0500

    add copyright

--- src/com/mebigfatguy/deadmethods/MethodCollectingVisitor.java
+++ src/com/mebigfatguy/deadmethods/MethodCollectingVisitor.java
@@ -1,3 +1,20 @@
+/*
+ * deadmethods - A unused methods detector
+ * Copyright 2011 MeBigFatGuy.com
+ * Copyright 2011 Dave Brosius
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and limitations
+ * under the License.
+ */
 package com.mebigfatguy.deadmethods;
 
 import java.util.HashSet;
@@ -12,7 +29,7 @@ import org.objectweb.asm.Opcodes;
 
 class MethodCollectingVisitor implements ClassVisitor {
     /**
-	 * 
+	 *
 	 */
 	private final FindDeadMethods findDeadMethods;
 
