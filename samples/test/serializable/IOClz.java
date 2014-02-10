504c04564051db60726ba8a7539858f986afe500
Dave Brosius <dbrosius@mebigfatguy.com>
2014-02-09 22:58:01 -0500

    make Serializable methods private, per spec

--- samples/test/serializable/IOClz.java
+++ samples/test/serializable/IOClz.java
@@ -10,15 +10,15 @@ public class IOClz implements Serializable {
 
     private static final long serialVersionUID = -8308067789551071759L;
 
-    protected void writeObject(ObjectOutputStream oos) {
+    private void writeObject(ObjectOutputStream oos) {
     }
     
-    protected void readObject(ObjectInputStream ois) {
+    private void readObject(ObjectInputStream ois) {
     }
     
-    protected void writeExternal(ObjectOutput oo) {
+    private void writeExternal(ObjectOutput oo) {
     }
     
-    protected void readExternal(ObjectInput oi) {
+    private void readExternal(ObjectInput oi) {
     }
 }
