eea5763db8856a9f6f428319788d389900f5a532
Dave Brosius <dbrosius@mebigfatguy.com>
2016-06-04 20:56:00 -0400

    unbury logic

--- src/com/mebigfatguy/deadmethods/MethodInfo.java
+++ src/com/mebigfatguy/deadmethods/MethodInfo.java
@@ -24,75 +24,76 @@ import org.objectweb.asm.Opcodes;
 
 public class MethodInfo {
 
-	private final String methodName;
-	private final String methodSignature;
-	private final int methodAccess;
-	private Set<String> annotations;
-	private boolean isTest;
+    private final String methodName;
+    private final String methodSignature;
+    private final int methodAccess;
+    private Set<String> annotations;
+    private boolean isTest;
 
-	public MethodInfo(String name, String signature, int access) {
-		methodName = name;
-		methodSignature = signature;
-		methodAccess = access;
-		isTest = false;
-	}
+    public MethodInfo(String name, String signature, int access) {
+        methodName = name;
+        methodSignature = signature;
+        methodAccess = access;
+        isTest = false;
+    }
 
-	public String getMethodName() {
-		return methodName;
-	}
+    public String getMethodName() {
+        return methodName;
+    }
 
-	public String getMethodSignature() {
-		return methodSignature;
-	}
+    public String getMethodSignature() {
+        return methodSignature;
+    }
 
-	public boolean isSynthetic() {
-		return (methodAccess & Opcodes.ACC_SYNTHETIC) != 0;
-	}
+    public boolean isSynthetic() {
+        return (methodAccess & Opcodes.ACC_SYNTHETIC) != 0;
+    }
 
-	public int getMethodAccess() {
-		return methodAccess;
-	}
+    public int getMethodAccess() {
+        return methodAccess;
+    }
 
-	public boolean isTest() {
-		return isTest;
-	}
+    public boolean isTest() {
+        return isTest;
+    }
 
-	public void setTest(boolean test) {
-		isTest = test;
-	}
-	
-	public void addAnnotation(String annotation) {
-	    if (annotations == null) {
-	        annotations = new HashSet<String>();
-	    }
-	    annotations.add(annotation);
-	}
-	
-	public boolean hasAnnotations() {
-	    return annotations != null;
-	}
-	
-	public boolean hasAnnotation(String annotation) {
-	    return (annotations != null) && annotations.contains(annotation);
-	}
+    public void setTest(boolean test) {
+        isTest = test;
+    }
 
-	@Override
-	public int hashCode() {
-		return methodName.hashCode() ^ methodSignature.hashCode();
-	}
+    public void addAnnotation(String annotation) {
+        if (annotations == null) {
+            annotations = new HashSet<String>();
+        }
+        annotations.add(annotation);
+    }
 
-	@Override
-	public boolean equals(Object o) {
-		if (o instanceof MethodInfo) {
-			MethodInfo that = (MethodInfo) o;
-			return methodName.equals(that.methodName)
-			    && methodSignature.equals(that.methodSignature)
-			  && ((methodAccess & Opcodes.ACC_STATIC) == (that.methodAccess & Opcodes.ACC_STATIC));
-		}
-		return false;
-	}
-	@Override
-	public String toString() {
-		return methodName + methodSignature;
-	}
+    public boolean hasAnnotations() {
+        return annotations != null;
+    }
+
+    public boolean hasAnnotation(String annotation) {
+        return (annotations != null) && annotations.contains(annotation);
+    }
+
+    @Override
+    public int hashCode() {
+        return methodName.hashCode() ^ methodSignature.hashCode();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof MethodInfo)) {
+            return false;
+        }
+
+        MethodInfo that = (MethodInfo) o;
+        return methodName.equals(that.methodName) && methodSignature.equals(that.methodSignature)
+                && ((methodAccess & Opcodes.ACC_STATIC) == (that.methodAccess & Opcodes.ACC_STATIC));
+    }
+
+    @Override
+    public String toString() {
+        return methodName + methodSignature;
+    }
 }
