f1dd101c4b3a2a051be5b525680cad28482405c7
Dave Brosius <dbrosius@mebigfatguy.com>
2017-05-28 15:16:51 -0400

    start working on a mvn plugin

---
+++ src/com/mebigfatguy/deadmethods/mvn/FDMMojo.java
@@ -0,0 +1,46 @@
+/*
+ * deadmethods - A unused methods detector
+ * Copyright 2011-2017 MeBigFatGuy.com
+ * Copyright 2011-2017 Dave Brosius
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and limitations
+ * under the License.
+ */
+package com.mebigfatguy.deadmethods.mvn;
+
+import java.io.File;
+
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
+import org.apache.maven.plugins.annotations.ResolutionScope;
+import org.apache.maven.project.MavenProject;
+
+import com.sun.scenario.Settings;
+
+@Mojo(name = "finddeadmethods", requiresDependencyResolution = ResolutionScope.COMPILE, threadSafe = true)
+public class FDMMojo extends AbstractMojo {
+
+    @Parameter(defaultValue = "${settings}", readonly = true, required = true)
+    private Settings settings;
+
+    @Parameter(defaultValue = "${project}", readonly = true, required = true)
+    MavenProject project;
+
+    @Parameter(property = "outputFile")
+    private File outputFile;
+
+    @Override
+    public void execute() throws MojoExecutionException {
+    }
+}
