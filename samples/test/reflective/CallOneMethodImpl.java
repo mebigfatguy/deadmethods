2616dbcc1b86881ca5213eee56b174ac8afd58b5
Dave Brosius <dbrosius@mebigfatguy.com>
2017-05-28 12:25:23 -0400

    warnings

--- samples/test/reflective/CallOneMethodImpl.java
+++ samples/test/reflective/CallOneMethodImpl.java
@@ -2,10 +2,10 @@ package test.reflective;
 
 public class CallOneMethodImpl {
 
-	public void useReflection(String clsName) throws Exception {	
-		Class c = Class.forName(clsName);
-		OneMethod o = (OneMethod) c.getConstructor(String.class).newInstance("Foo");
-		
-		o.oneMethod();
-	}
+    public void useReflection(String clsName) throws Exception {
+        Class<?> c = Class.forName(clsName);
+        OneMethod o = (OneMethod) c.getConstructor(String.class).newInstance("Foo");
+
+        o.oneMethod();
+    }
 }
