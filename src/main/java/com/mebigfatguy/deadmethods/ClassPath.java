571e5aca7ca1e57e7d27bcc8ba12e5987c698246
Dave Brosius <dbrosius@mebigfatguy.com>
2019-01-06 20:06:20 -0500

    Happy New Year

--- src/main/java/com/mebigfatguy/deadmethods/ClassPath.java
+++ src/main/java/com/mebigfatguy/deadmethods/ClassPath.java
@@ -1,7 +1,7 @@
 /*
  * deadmethods - A unused methods detector
- * Copyright 2011-2018 MeBigFatGuy.com
- * Copyright 2011-2018 Dave Brosius
+ * Copyright 2011-2019 MeBigFatGuy.com
+ * Copyright 2011-2019 Dave Brosius
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
