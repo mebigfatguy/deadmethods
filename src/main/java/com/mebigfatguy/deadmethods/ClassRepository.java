19eb126dcfbecbc43f481c47787404062d140a3c
Dave Brosius <dbrosius@mebigfatguy.com>
2023-11-12 16:03:36 -0500

    increase thread priority of ClassScanner threads

--- src/main/java/com/mebigfatguy/deadmethods/ClassRepository.java
+++ src/main/java/com/mebigfatguy/deadmethods/ClassRepository.java
@@ -17,8 +17,6 @@
  */
 package com.mebigfatguy.deadmethods;
 
-import org.objectweb.asm.ClassReader;
-
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
@@ -27,11 +25,21 @@ import java.net.URL;
 import java.net.URLClassLoader;
 import java.security.AccessController;
 import java.security.PrivilegedAction;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadFactory;
 import java.util.concurrent.atomic.AtomicBoolean;
+
+import org.objectweb.asm.ClassReader;
 
 public class ClassRepository implements Iterable<String> {
 
@@ -60,7 +68,16 @@ public class ClassRepository implements Iterable<String> {
         } catch (IOException e) {
         }
 
-        executor = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors() * 2);
+        executor = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors() * 2,  new ThreadFactory() {
+        	
+			@Override
+			public Thread newThread(Runnable r) {
+				Thread t = new Thread(r);
+				t.setPriority(Thread.MAX_PRIORITY-1);
+				return t;
+			}
+        	
+        });
         executor.execute(new ClassPopulator());
 
         try {
